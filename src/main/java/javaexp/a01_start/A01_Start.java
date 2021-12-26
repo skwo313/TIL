f306bc3a6b7b9d7c0fcbb38237ba41dcb67961e2
Gwangwoo Seok <skwo319@naver.com>
2021-12-26 23:03:34 +0900

    1226 commit

--- src/main/java/javaexp/a01_start/A01_Start.java
+++ src/main/java/javaexp/a01_start/A01_Start.java
@@ -5,7 +5,10 @@ public class A01_Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요! 드디어 자바시작입니다!");
-		
+		show();
+		show();
 	}
-
+	static void show(){
+        System.out.println("강한친구 대한육군");
+    }
 }
