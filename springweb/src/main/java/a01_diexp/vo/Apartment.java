de6830e44afb1e6339e60b2c74113aa85dba390c
Gwangwoo Seok <skwo319@naver.com>
2022-01-25 18:19:43 +0900

    0125 commit

---
+++ springweb/src/main/java/a01_diexp/vo/Apartment.java
@@ -0,0 +1,29 @@
+package a01_diexp.vo;
+
+public class Apartment {
+	private String dong;
+	private String hosu;
+	public Apartment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Apartment(String dong, String hosu) {
+		super();
+		this.dong = dong;
+		this.hosu = hosu;
+	}
+	public String getDong() {
+		return dong;
+	}
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
+	public String getHosu() {
+		return hosu;
+	}
+	public void setHosu(String hosu) {
+		this.hosu = hosu;
+	}
+	
+	
+}
