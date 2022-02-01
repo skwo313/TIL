54fe99b5c8ef3d73ea22f4ed11dde16ce5974c91
Gwangwoo Seok <skwo319@naver.com>
2022-02-01 18:40:35 +0900

    0201 commit

--- src/main/java/javaexp/a00_exp/Main.java
+++ src/main/java/javaexp/a00_exp/Main.java
@@ -1,22 +1,29 @@
 package javaexp.a00_exp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			int T = Integer.parseInt(br.readLine());
-			for(int i=1;i<=T;i++) {
-				int A = Integer.parseInt(br.readLine());
-				int B = Integer.parseInt(br.readLine());
-				System.out.println(A+B);
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 종류
+		int K = sc.nextInt(); // 가치의 합
+		
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		int coin = 1;
+		for(int i = 0; i<N; i++) {
+			if(i == 0) {
+				array.add(coin);
+			}else if(i%2 == 1) {
+				coin = coin*5;
+				array.add(coin);
+			} else {
+				coin = coin*2;
+				array.add(coin);
 			}
-		} catch (IOException e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
+		}
+		for(int i = 0;i<array.size();i++) {
+			if(array.get(i) < K && )
 		}
 	}
 }
